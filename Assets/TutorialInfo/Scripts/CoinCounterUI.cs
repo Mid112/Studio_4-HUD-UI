41542ca0e16a880413bfa51a83e34a3d217816d4
Mridul Nohria <mridulnohria2004@gmail.com>
2025-03-12 16:16:37 -0700

    Implemented animated coin counter

--- Assets/TutorialInfo/Scripts/CoinCounterUI.cs
+++ Assets/TutorialInfo/Scripts/CoinCounterUI.cs
@@ -1,5 +1,5 @@
 using TMPro;
-using DG.Tweening;  
+using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 
@@ -7,29 +7,41 @@ using UnityEngine;
 public class CoinCounterUI : MonoBehaviour
 {
 
-[SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI current;
 
 
-[SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private TextMeshProUGUI toUpdate;
 
 
-[SerializeField] private Transform coinTextContainer;
+    [SerializeField] private Transform coinTextContainer;
 
-[SerializeField] private float duration;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease animationCurve;
 
-private float containerInitPosition;
-private float moveAmount;
+    private float containerInitPosition;
+    private float moveAmount;
 
-private void Start()
-{
-    Canvas.ForceUpdateCanvases();
-    current.SetText("0");
-    toUpdate.SetText("0");
-    containerInitPosition = coinTextContainer.localPosition.y;
-    moveAmount = current.rectTransform.rect.height;
-}
-public void UpdateScore(int score){
-    toUpdate.SetText($"{score}");
-    coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
-}
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+    public void UpdateScore(int score)
+    {
+        toUpdate.SetText($"{score}");
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
+        StartCoroutine(ResetCoinContainer(score));
+    }
+    public IEnumerator ResetCoinContainer(int score)
+    {
+        yield return new WaitForSeconds(duration);
+        current.SetText($"{score}");
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+    }
+
+
 }
